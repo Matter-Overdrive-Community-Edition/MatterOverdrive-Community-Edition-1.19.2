457e1a94d90100666b497d536000a48e75843e0f
infiniteblock <subaru_gt@windowslive.com>
2024-02-18 18:24:39 +1300

    Update OverdriveBlockTagsProvider.java

--- src/main/java/matteroverdrive/datagen/server/OverdriveBlockTagsProvider.java
+++ src/main/java/matteroverdrive/datagen/server/OverdriveBlockTagsProvider.java
@@ -24,14 +24,21 @@ public class OverdriveBlockTagsProvider extends BlockTagsProvider {
 				.add(BlockRegistry.BLOCK_FLOOR_TILES.<Block>getObjectsAsArray(new Block[0]))
 				.add(BlockRegistry.BLOCK_TRITANIUM_CRATES.<Block>getObjectsAsArray(new Block[0]))
 				.add(BlockRegistry.BLOCK_SOLAR_PANEL.get()).add(BlockRegistry.BLOCK_MATTER_DECOMPOSER.get())
+				.add(BlockRegistry.BLOCK_DECORATIVE_BEAMS.get()).add(BlockRegistry.BLOCK_DECORATIVE_CARBON_FIBER_PLATE.get())
+				.add(BlockRegistry.BLOCK_DECORATIVE_CLEAN.get()).add(BlockRegistry.BLOCK_DECORATIVE_COILS.get())
+				.add(BlockRegistry.BLOCK_MACHINE_HULL.get()).add(BlockRegistry.BLOCK_TRITANIUM.get())
 				.add(BlockRegistry.BLOCK_MATTER_RECYCLER.get()).add(BlockRegistry.BLOCK_CHARGER.get())
 				.add(BlockRegistry.BLOCK_MICROWAVE.get()).add(BlockRegistry.BLOCK_TRANSPORTER.get())
 				.add(BlockRegistry.BLOCK_VENT_OPEN.get()).add(BlockRegistry.BLOCK_VENT_CLOSED.get())
-				.add(BlockRegistry.BLOCK_SPACETIME_ACCELERATOR.get())
+				.add(BlockRegistry.BLOCK_ANDROID_STATION.get()).add(BlockRegistry.BLOCK_WEAPON_STATION.get())
+				.add(BlockRegistry.BLOCK_SPACETIME_ACCELERATOR.get()).add(BlockRegistry.BLOCK_STAR_MAP.get())
 				.add(BlockRegistry.BLOCK_MATTER_NETWORK_CABLES.getObjectsAsArray(new Block[0]))
 				.add(BlockRegistry.BLOCK_CHUNKLOADER.get()).add(BlockRegistry.BLOCK_MATTER_ANALYZER.get())
 				.add(BlockRegistry.BLOCK_PATTERN_STORAGE.get()).add(BlockRegistry.BLOCK_PATTERN_MONITOR.get())
-				.add(BlockRegistry.BLOCK_MATTER_REPLICATOR.get());
+				.add(BlockRegistry.BLOCK_MATTER_REPLICATOR.get()).add(BlockRegistry.DILITHIUM_ORE.get())
+				.add(BlockRegistry.TRITANIUM_ORE.get()).add(BlockRegistry.DEEPSLATE_TRITANIUM_ORE.get())
+				.add(BlockRegistry.DILITHIUM_ORE.get()).add(BlockRegistry.DEEPSLATE_DILITHIUM_ORE.get())
+				.add(BlockRegistry.BLOCK_NETWORK_SWITCH.get()).add(BlockRegistry.BLOCK_INSCRIBER.get());
 
 		tag(BlockTags.NEEDS_STONE_TOOL).add(BlockRegistry.BLOCK_REGULAR_TRITANIUM_PLATING.get())
 				.add(BlockRegistry.BLOCK_COLORED_TRITANIUM_PLATING.<Block>getObjectsAsArray(new Block[0]))
