22e51f3459ec7ff369ad738f8a627aba639e5644
infiniteblock <subaru_gt@windowslive.com>
2024-03-08 20:33:38 +1300

    Update ScreenTritaniumCrate.java
    Fix chest Inventory text

--- src/main/java/matteroverdrive/client/screen/ScreenTritaniumCrate.java
+++ src/main/java/matteroverdrive/client/screen/ScreenTritaniumCrate.java
@@ -1,5 +1,7 @@
 package matteroverdrive.client.screen;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+
 import matteroverdrive.common.inventory.InventoryTritaniumCrate;
 import matteroverdrive.core.screen.types.GenericVanillaScreen;
 import net.minecraft.network.chat.Component;
@@ -11,5 +13,9 @@ public class ScreenTritaniumCrate extends GenericVanillaScreen<InventoryTritaniu
 		super(pMenu, pPlayerInventory, pTitle, 176 + InventoryTritaniumCrate.OFFSET, 166 + InventoryTritaniumCrate.OFFSET);
 	}
 
-	
+	 @Override
+	  protected void renderLabels(PoseStack matrixStack, int mouseX, int mouseY) {
+	    this.font.draw(matrixStack, this.title, 8.0F, 6.0F, 4210752);
+	    this.font.draw(matrixStack, this.playerInventoryTitle, 8.0F, (float) (this.imageHeight - 96 + 2), 4210752);
+	  }
 }
